2fd833f30fe773e5f7ace3e66bab8d0d646110ab
6jiaxi0 <2104960955@qq.com>
2025-03-26 13:25:13 +0800

    更改信息

--- src/com/LJX/test/git.java
+++ src/com/LJX/test/git.java
@@ -2,6 +2,6 @@ package com.LJX.test;
 
 public class git {
     public static void main(String[] args) {
-        System.out.println("happy World");
+        System.out.println("pain World");
     }
 }
